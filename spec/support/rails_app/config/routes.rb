8fd8edbfb61c067952cb34e79be761b7d7c40908
Vitor Oliveira <vbrazo@gmail.com>
2017-01-17 15:36:08 -0200

    Move dummy app project to spec/support

--- spec/support/rails_app/config/routes.rb
+++ spec/support/rails_app/config/routes.rb
@@ -1,4 +1,3 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  mount AuditTables::Engine => '/audit_tables'
 end
---
+++ spec/support/rails_app/db/migrate/20170117165956_create_users.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :name, null: false
+
+      t.timestamps
+    end
+  end
+end
