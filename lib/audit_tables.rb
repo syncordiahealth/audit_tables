ce4996543c8a555f01a6ecc63914d5c6ff1a9144
Vitor Oliveira <vbrazo@gmail.com>
2017-01-17 00:02:56 -0200

    Test against local copy of the gem and change methods

--- lib/audit_tables.rb
+++ lib/audit_tables.rb
@@ -1,24 +1,32 @@
 # frozen_string_literal: true
-require 'audit_tables/version'
+require 'base'
+require 'build_audit_trigger'
+require 'change_audit_table'
+require 'create_new_audit_table'
+require 'create_audit_tables_for_existing_tables'
 
 module AuditTables
-  def create_audit_table_for(table_name)
+  def self.create_audit_table_for(table_name)
     AuditTables::CreateNewAuditTable.new(table_name.to_s).build
   end
 
-  def create_audit_tables_for_existing_tables(options = {})
+  def self.create_audit_tables_for_existing_tables(options = {})
     AuditTables::CreateAuditTablesForExistingTables.new(options[:excluded_tables]).process
   end
 
-  def change_audit_table_for(table_name)
+  def self.change_audit_table_for(table_name)
     AuditTables::ChangeAuditTable.new(table_name.to_s).execute
   end
 
-  def build_audit_triggers_for(table_name)
+  def self.build_audit_triggers_for(table_name)
     AuditTables::BuildAuditTrigger.new(table_name.to_s).build
   end
 
-  def rebuild_all_audit_triggers(options = {})
+  def self.awesome?
+    puts 'awesome!!'
+  end
+
+  def self.rebuild_all_audit_triggers(options = {})
     tables = ActiveRecord::Base.connection.tables
     tables -= options[:excluded_tables]
 
